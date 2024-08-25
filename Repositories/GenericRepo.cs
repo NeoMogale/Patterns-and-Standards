0abe85bbfc544f36ecada766a947229490699586
Neo Mogale <neophillipinemogale@gmail.com>
2024-08-25 07:08:08 +0200

    Update GenericRepo.cs

--- Repositories/GenericRepo.cs
+++ Repositories/GenericRepo.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using TelemetryPortal_MVC.Data;
 
+// DbContext referenced in this class
 
 namespace TelemetryPortal_MVC.Repositories
 {
